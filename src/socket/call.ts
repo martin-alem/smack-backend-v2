18ae98eaec1d3f18856af8c44566188b90ea4d9e
Martin Alemajoh <alemajohmartin@gmail.com>
2022-01-26 14:49:31 -0500

    register events for handling webrtc signalling

--- src/socket/call.ts
+++ src/socket/call.ts
@@ -7,34 +7,33 @@ function call(io: Server, socket: Socket) {
   });
 
   socket.on("answer_call", payload => {
-    const { userId } = payload;
-    socket.broadcast.to(userId).emit("answer_call", payload);
+    const { callerId } = payload;
+    socket.broadcast.to(callerId).emit("answer_call", payload);
   });
 
   socket.on("reject_call", payload => {
-    const { userId } = payload;
-    socket.broadcast.to(userId).emit("reject_call", payload);
+    const { callerId } = payload;
+    socket.broadcast.to(callerId).emit("reject_call", payload);
   });
 
   socket.on("sdp-offer", payload => {
-    const { peer } = payload;
-    socket.broadcast.to(peer).emit("sdp-offer", payload);
+    const { peerId } = payload;
+    socket.broadcast.to(peerId).emit("sdp-offer", payload);
   });
 
   socket.on("sdp-answer", payload => {
-    const { peer } = payload;
-    socket.broadcast.to(peer).emit("sdp-answer", payload);
+    const { peerId } = payload;
+    socket.broadcast.to(peerId).emit("sdp-answer", payload);
   });
 
   socket.on("ice-candidate", payload => {
-    const { peer } = payload;
-    socket.broadcast.to(peer).emit("ice-candidate", payload);
+    const { peerId } = payload;
+    socket.broadcast.to(peerId).emit("ice-candidate", payload);
   });
 
   socket.on("leave", payload => {
-    const { peer } = payload;
-    console.log(payload);
-    socket.broadcast.to(peer).emit("leave", payload);
+    const { peerId } = payload;
+    socket.broadcast.to(peerId).emit("leave", payload);
   });
 }
 
